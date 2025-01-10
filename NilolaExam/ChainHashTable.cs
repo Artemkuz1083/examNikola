331db83a252d0f79f0c132ee94443aa1b72603f9
Artem Kuznetsov <artemkuz1083@gmail.com>
2025-01-10 21:29:36 +0500

    ChainHashTable

---
+++ NilolaExam/ChainHashTable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NilolaExam
+{
+    public class ChainHashTable<K,V>
+    {
+        private int _size;
+        private LinkedList<KeyValuePair<K, V>>[] _table;
+
+        public ChainHashTable(int size)
+        {
+            _size = size;
+            _table = new LinkedList<KeyValuePair<K, V>>[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                _table[i] = new LinkedList<KeyValuePair<K, V>>();
+            }
+        }
+
+        public int GetIndex(K key)
+        {
+            return key.GetHashCode() % _size;
+        }
+
+        public void Insert(K key, V value)
+        {
+            int index = GetIndex(key);
+            _table[index].AddFirst(new KeyValuePair<K, V>(key, value));
+        }
+
+        public V Search(K key)
+        {
+            int index = GetIndex(key);
+
+            foreach(var pair in _table[index])
+            {
+                if (pair.Key.Equals(key))
+                {
+                    return pair.Value;
+                }
+            }
+
+            throw new KeyNotFoundException();
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < _table.Length; i++)
+            {
+                Console.Write($"[ {i}: ");
+                if (_table[i] != null && _table[i].Count > 0)
+                {
+                    foreach (var kvp in _table[i])
+                    {
+                        Console.Write($"({kvp.Key}: {kvp.Value}) -> ");
+                    }
+                    Console.WriteLine("null ]");
+                }
+                else
+                {
+                    Console.WriteLine("... ]");
+                }
+            }
+        }
+
+    }
+}
--- NilolaExam/TestClass.cs
+++ NilolaExam/TestClass.cs
@@ -11,9 +11,14 @@ namespace NilolaExam
 
         public static void Main()
         {
-            var testArray = new[] {3,1,9,8,11,6};
-            Console.WriteLine(String.Join(" ",Shell.ShellSort(testArray)));
-
+            //var testArray = new[] {3,1,9,8,11,6};
+            //Console.WriteLine(String.Join(" ",Shell.ShellSort(testArray)));
+            var table = new ChainHashTable<int, int>(5);
+            table.Insert(0, 1);
+            table.Insert(0, 1);
+            table.Insert(0, 2);
+            table.Insert(3, 3);
+            table.Print();
         }
     }
 }
